608d6756ce10a7670459b1f593cb5a339efb3ba1
kpop1106 <phxsunssn13@yahoo.com.tw>
2015-05-06 13:10:53 +0800

    1

---
+++ src/model/DefaultFactory.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.*;
+
+public class DefaultFactory {
+	private static SessionFactory sessionFactory;
+	private static ServiceRegistry serviceRegistry;
+
+	@SuppressWarnings("deprecation")
+	private static SessionFactory configureSessionFactory()
+			throws HibernateException {
+		if (sessionFactory != null)
+			return sessionFactory;
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		serviceRegistry = new ServiceRegistryBuilder().applySettings(
+				configuration.getProperties()).buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		return sessionFactory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return configureSessionFactory();
+	}
+
+	public static void shutdown() {
+		sessionFactory.close();
+	}
+}
